10866fabcdf987a6f03379e3dc547d524d62f82e
Eric Anicet <anicetkeric@gmail.com>
2017-05-11 16:24:21 +0000

    initial commit

---
+++ WcfService/BookService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "BookService" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez BookService.svc ou BookService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class BookService : IBookService
+    {
+        public void DoWork()
+        {
+        }
+
+        public List<Book> GetAll()
+        {
+            return new BookManager().GetAllBooks();
+        }
+
+        public Book AddBook(Book newBook)
+        {
+            return new BookManager().AddBook(newBook);
+        }
+
+        public Book GetBook(string id)
+        {
+
+            return new BookManager().GetBook(Int32.Parse(id));
+
+        }
+
+        public List<Book> DeleteBook(string idBook)
+        {
+            return new BookManager().DeleteBook(Int32.Parse(idBook));
+        }
+
+        public Book UpdateBook(Book newBook)
+        {
+            return new BookManager().UpdateBook(newBook);
+        }
+    }
+}
---
+++ WcfService/IBookService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IBookService" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IBookService
+    {
+        [OperationContract]
+        void DoWork();
+
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+                UriTemplate = "/api/book/GetAllBooks/",
+                RequestFormat = WebMessageFormat.Json,
+                ResponseFormat = WebMessageFormat.Json)
+            ]
+        List<Book> GetAll();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+          UriTemplate = "/api/book/addBook/",
+          RequestFormat = WebMessageFormat.Json,
+          ResponseFormat = WebMessageFormat.Json)
+      ]
+        Book AddBook(Book newBook);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+       UriTemplate = "api/book/GetBook/{id}",
+       RequestFormat = WebMessageFormat.Json,
+       ResponseFormat = WebMessageFormat.Json)
+   ]
+        Book GetBook(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        UriTemplate = "api/book/DeleteBook/{idBook}",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json)
+        ]
+        List<Book> DeleteBook(string idBook);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+        UriTemplate = "api/book/UpdateBook/",
+        RequestFormat = WebMessageFormat.Json,
+        ResponseFormat = WebMessageFormat.Json)
+        ]
+        Book UpdateBook(Book newBook);
+
+    }
+}
